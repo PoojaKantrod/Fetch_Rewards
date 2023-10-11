94f822be4bc8ac3a030a5c9d43d7a6171e5e0c1f
Ankit Patil <apatil44@hawk.iit.edu>
2023-10-11 16:45:37 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/fetch_rewards_assessment/ID.java
@@ -0,0 +1,32 @@
+package com.example.fetch_rewards_assessment;
+
+
+
+public class ID {
+    String id;
+    String name;
+
+    public ID(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public ID() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
